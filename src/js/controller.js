c4c4e7ce709d35a458df045459f797aa7389a980
MartinSkutin <martinskutin@gmail.com>
2022-10-05 13:46:57 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -121,6 +121,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -129,5 +133,6 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
