3897f584a5346007656d73d99b9ad2d21f2f4752
MartinSkutin <martinskutin@gmail.com>
2022-10-05 13:06:09 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '22704efd-1041-472a-b8bd-db85f71343c2';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,66 @@
+import icons from 'url:../../img/icons.svg';
+import View from './View.js';
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkup() {
+    const curPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+
+    // Page 1, and there are other pages
+    if (curPage === 1 && numPages > 1) {
+      return this._generateMarkupButton('next', curPage);
+    }
+
+    // Last page
+    if (curPage === numPages && numPages > 1) {
+      return this._generateMarkupButton('prev', curPage);
+    }
+    // Other page
+    if (curPage < numPages) {
+      return this._generateMarkupButton('all', curPage);
+    }
+    // Page 1, and there are NO other pages
+    return '';
+  }
+
+  _generateMarkupButton(btn, curPage) {
+    const prev = `
+        <button data-goto="${
+          curPage - 1
+        }" class="btn--inline pagination__btn--prev">
+              <svg class="search__icon">
+                <use href="${icons}#icon-arrow-left"></use>
+              </svg>
+              <span>Page ${curPage - 1}</span>
+        </button>`;
+    const next = `  <button data-goto="${
+      curPage + 1
+    }" class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>`;
+
+    if (btn === 'prev') return prev;
+    if (btn === 'next') return next;
+    if (btn === 'all') return prev + next;
+  }
+}
+
+export default new PaginationView();
